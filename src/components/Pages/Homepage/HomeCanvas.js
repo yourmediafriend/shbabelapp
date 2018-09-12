dbf4bb4d09596b03b466224b668025f3a71c791f
John Walker <yourmediafriend@gmail.com>
2018-09-12 09:33:00 +0100

    Pixi Not working

--- src/components/Pages/Homepage/HomeCanvas.js
+++ src/components/Pages/Homepage/HomeCanvas.js
@@ -8,16 +8,38 @@ import PIXI from 'pixi.js';
 const Sprite = React.createFactory(ReactPIXI.Sprite);
 const DisplayObjectContainer = React.createFactory(ReactPIXI.DisplayObjectContainer);
 
-const CupcakeComponent = React.createClass({
-  displayName: 'CupcakeComponent',
-  // maps from cupcake toppings to the appropriate sprite
-  spritemapping : {
-    'vanilla' : assetpath('creamVanilla.png'),
-    'chocolate' : assetpath('creamChoco.png'),
-    'mocha' : assetpath('creamMocha.png'),
-    'pink' : assetpath('creamPink.png'),
-  },
-  render : function () {
+// const CupcakeComponent = React.createClass({
+//   displayName: 'CupcakeComponent',
+//   // maps from cupcake toppings to the appropriate sprite
+//   spritemapping : {
+//     'vanilla' : assetpath('creamVanilla.png'),
+//     'chocolate' : assetpath('creamChoco.png'),
+//     'mocha' : assetpath('creamMocha.png'),
+//     'pink' : assetpath('creamPink.png'),
+//   },
+//   render : function () {
+//     let creamimagename = this.spritemapping[this.props.topping];
+//     let xposition = this.props.xposition;
+//     return DisplayObjectContainer(
+//       {x:xposition, y:100 },
+//       Sprite({image:creamimagename, y:-35, anchor: new PIXI.Point(0.5,0.5), key:'topping'}, null),
+//       Sprite({image:assetpath('cupCake.png'), y:35, anchor: new PIXI.Point(0.5,0.5), key:'cake'}, null)
+//     );
+//   }
+// });
+
+const assetpath = (filename) => { return '../../media/pixi/' + filename; };
+
+const spritemapping = {
+  'vanilla' : assetpath('creamVanilla.png'),
+  'chocolate' : assetpath('creamChoco.png'),
+  'mocha' : assetpath('creamMocha.png'),
+  'pink' : assetpath('creamPink.png'),
+}
+
+class CupcakeComponent extends Component {
+
+  render() {
     let creamimagename = this.spritemapping[this.props.topping];
     let xposition = this.props.xposition;
     return DisplayObjectContainer(
@@ -26,14 +48,13 @@ const CupcakeComponent = React.createClass({
       Sprite({image:assetpath('cupCake.png'), y:35, anchor: new PIXI.Point(0.5,0.5), key:'cake'}, null)
     );
   }
-});
-
+}
 
 class HomeCanvas extends Component {
   render() {
     return (
       <div>
-<CupcakeComponent />
+        <CupcakeComponent topping='chocolate' />
       </div>
     )
   }
