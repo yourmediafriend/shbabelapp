c3b00b7b6da666d8a1d766c09d9597d89299427c
John Walker <yourmediafriend@gmail.com>
2018-09-12 01:45:36 +0100

    Adding Easeljs

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -94,7 +94,6 @@ module.exports = {
       // Support React Native Web
       // https://www.smashingmagazine.com/2016/08/a-glimpse-into-the-future-with-react-native-for-web/
       'react-native': 'react-native-web',
-
       "TweenLite": path.resolve('node_modules', 'gsap/src/uncompressed/TweenLite.js'),
       "TweenMax": path.resolve('node_modules', 'gsap/src/uncompressed/TweenMax.js'),
       "TimelineLite": path.resolve('node_modules', 'gsap/src/uncompressed/TimelineLite.js'),
