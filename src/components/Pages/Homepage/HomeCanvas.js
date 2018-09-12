b111f36ce3d1a25245e96fa634e6166facb35bec
John Walker <yourmediafriend@gmail.com>
2018-09-12 23:53:52 +0100

    Pixi Update

--- src/components/Pages/Homepage/HomeCanvas.js
+++ src/components/Pages/Homepage/HomeCanvas.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import cx from 'classnames';
 import styles from './homepage.scss'
 
-import { Stage, Sprite, Container } from '@inlet/react-pixi'
+import { Stage, Sprite, Container, TilingSprite, Text } from '@inlet/react-pixi'
 import * as PIXI from 'pixi.js';
 
 import cat from './media/cat.png'
@@ -17,7 +17,27 @@ let factory_3 = "https://res.cloudinary.com/dghff7rpa/image/upload/v1536613725/b
 
 const bgImagesFactories = [factory_1, factory_2, factory_3];
 
+let bgVideo = 'https://res.cloudinary.com/dghff7rpa/video/upload/v1536614709/backgrounds/video/abstract_bw.mp4';
+
+
 PIXI.settings.SCALE_MODE = PIXI.SCALE_MODES.NEAREST;
+
+
+const textBanner = ['Degredation','Exploitation','Despair'];
+
+class SpriteVideo extends Component {
+
+  render() {
+    return (
+      <Sprite
+        texture={PIXI.Texture.fromVideo(bgVideo)}
+        width={window.innerWidth}
+        height={window.innerHeight}
+        autoPlay={true}
+      />
+    );
+  }
+}
 
 
 class SpriteTest extends Component {
@@ -44,7 +64,7 @@ class SpriteTest extends Component {
   render() {
     return (
       <Sprite
-        texture={PIXI.Texture.fromImage(bgImages[this.props.activeSceneId])}
+        texture={PIXI.Texture.fromImage(bgImagesFactories[this.props.activeSceneId])}
         width={window.innerWidth}
         height={window.innerHeight}
         x={window.innerWidth/2}
@@ -57,11 +77,64 @@ class SpriteTest extends Component {
   }
 }
 
+
+class TilingSpriteTest extends Component {
+  render() {
+    return (
+      <TilingSprite
+        texture={PIXI.Texture.fromImage(bgImages[this.props.activeSceneId])}
+        width={window.innerWidth}
+        height={window.innerHeight}
+        tilePosition={{ x: 10, y: 10 }}
+        tileScale={{ x: 0.2, y: 0.2 }}
+      />
+    );
+  }
+}
+
+class TextTest extends Component {
+  render() {
+    return (
+      <Text
+        text={textBanner[this.props.activeSceneId]}
+        anchor={0.5}
+        x={window.innerWidth/2}
+        y={window.innerHeight/2}
+        style={
+          new PIXI.TextStyle({
+            align: 'center',
+            fontFamily: '"Source Sans Pro", Helvetica, sans-serif',
+            fontSize: 50,
+            fontWeight: 400,
+            fill: ['#ffffff', '#00ff99'], // gradient
+            stroke: '#01d27e',
+            strokeThickness: 5,
+            letterSpacing: 20,
+            dropShadow: true,
+            dropShadowColor: '#ccced2',
+            dropShadowBlur: 4,
+            dropShadowAngle: Math.PI / 6,
+            dropShadowDistance: 6,
+            wordWrap: true,
+            wordWrapWidth: 440,
+          })
+        }
+      />
+    );
+  }
+}
+
+
+
+
 class CanvasTest extends Component {
   render() {
     return (
       <Stage width={window.innerWidth} height={window.innerHeight}  options={{ backgroundColor: 0x343434 }}>
-        <SpriteTest activeSceneId={this.props.activeSceneId} />
+        <SpriteVideo />
+{/*        <SpriteTest activeSceneId={this.props.activeSceneId} />
+        <TilingSpriteTest activeSceneId={this.props.activeSceneId} />
+        <TextTest activeSceneId={this.props.activeSceneId} />*/}
       </Stage>
     );
   }
