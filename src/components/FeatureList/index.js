5bb9586aa17840e301f505d64751e08ba096d748
John Walker <yourmediafriend@gmail.com>
2018-10-10 12:16:46 +0100

    LIst with animation but ql list saved

--- src/components/FeatureList/index.js
+++ src/components/FeatureList/index.js
@@ -3,20 +3,18 @@ import styles from './styles.scss';
 import ScrollMagicEnhanced from './scrollMagicEnhanced';
 import { Row, Col, ListGroup, ListGroupItem } from 'reactstrap';
 import cx from 'classnames'
-import { Query } from "react-apollo";
-import featureListQuery from '../../graphQL/featureListQuery';
 
 const FeatureListItem = props => {
 
-  const { content } = props;
   return (
     <ListGroupItem className={cx(styles.listGroupItem)} data-delay={Math.round(props.delay * 100) / 100}>
       <div>
         <img src="http://www.pixijs.com/wp/wp-content/uploads/feature-multiplatform.png"
              alt="Multi-platform Support graphic"  className={cx(styles.img,'img')} />
         <div className={cx(styles.text,'text')}>
-          <h6 className="bold"><span>{content.title}</span></h6>
-          {content.fieldBody ? <p>{content.fieldBody}</p> : ''}
+          <h6 className="bold">Multi-platform Support</h6>
+          <p className="big-text">Interactive, visually compelling content on desktop, mobile and beyond, all
+            reached with a single codebase to deliver transferable experiences.</p>
         </div>
       </div>
     </ListGroupItem>
@@ -26,25 +24,18 @@ const FeatureListItem = props => {
 
 const FeatureList = props => {
 
+  const data = [1,2,3,4,5,6,7,8,9,10,11,12,13];
   const delay = 1;
 
   return (
-    <Query query={featureListQuery} >
-      {({ loading, error, data }) => {
-        if (loading) return <p>Loading...</p>;
-        if (error) return `Error: ${error.message}`;
-        if (data.nodeQuery.entities.length) {
-          return (
-            <ListGroup id='FeatureList' className={styles.listGroup}  >
-              {data.nodeQuery.entities.map((item, index) => <FeatureListItem key={index} content={item}  delay={index * delay} />)}
-            </ListGroup>
-          )
-        }
-        return null; // replace this with something relevant
-      }}
-    </Query>
-  )
+    <ListGroup id='FeatureList' className={styles.listGroup}  >
 
+      {data.map((item, index) => {
+        return (<FeatureListItem key={index} delay={index * delay}/>);
+      })}
+
+    </ListGroup>
+  );
 }
 
 export default ScrollMagicEnhanced(FeatureList);
